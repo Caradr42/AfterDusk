bfd69ca29b2f36462aa308b258a3b38ed5807bbe
josegzza <a01038061@itesm.mx>
2019-04-09 22:43:29 -0500

    Adding tool entity

---
+++ src/ECS/Components/Tool.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ECS.Components;
+import ECS.Component;
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ *
+ * @author pepe_
+ */
+public class Tool extends Component{
+    
+    public ArrayList<Integer> activos;
+    int pasiva;
+    public ArrayList<Integer> activosDesbloqueados ;
+
+    public Tool(ArrayList<Integer> activos, int pasiva, ArrayList<Integer> activosDesbloqueados) {
+        this.activos = activos;
+        this.pasiva = pasiva;
+        this.activosDesbloqueados = activosDesbloqueados;
+    }
+
+    
+    
+}
