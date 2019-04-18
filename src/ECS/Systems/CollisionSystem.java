f45d398d67f4ab5e5d2c0d520b2ba507bfd14476
PabloMoreno2000 <pamta2000@gmail.com>
2019-04-17 20:46:32 -0500

    CollisionSystem seems to be working right

--- src/ECS/Systems/CollisionSystem.java
+++ src/ECS/Systems/CollisionSystem.java
@@ -13,6 +13,9 @@ import ECS.Entity;
 import ECS.SystemJob;
 import Scene.Scene;
 import Utility.Point;
+import java.awt.Rectangle;
+import static java.lang.Integer.min;
+import static java.lang.Math.abs;
 import java.util.ArrayList;
 
 /**
@@ -26,9 +29,6 @@ public class CollisionSystem extends SystemJob{
     
     //A playable must have a collidable assigned by reference
     private Playable playable;
-    
-    //collision margin in pixels
-    private static double colMargin = 5;
     
     private ArrayList<Integer> arrPlayables;
     
@@ -104,18 +104,6 @@ public class CollisionSystem extends SystemJob{
         Transform transformj = new Transform();
         Collidable collidablej = new Collidable();
         
-        //boolean to know if there is the first entity collides being at the right of b
-        Boolean bRight;
-        
-        //boolean to know if there is the first entity collides being at the right of b
-        Boolean bLeft;
-        
-        //boolean to know if there is the first entity collides being at the right of b
-        Boolean bUp;
-        
-        //boolean to know if there is the first entity collides being at the right of b
-        Boolean bDown;
-        
         //Getting th collidable and the tranform of the first entity
         Entity e = scene.entityManager.getEntityByID(i);
         collidablei = scene.entityManager.getEntityComponentInstance(i, collidablei.getClass());
@@ -126,12 +114,15 @@ public class CollisionSystem extends SystemJob{
         collidablej = scene.entityManager.getEntityComponentInstance(j, collidablej.getClass());
         transformj = scene.entityManager.getEntityComponentInstance(j, transformj.getClass());
 
+        //Rectangle of the first entity
+        Rectangle firstRect = new Rectangle((int)transformi.position.x, (int)transformi.position.y, (int)collidablei.hitbox.x, (int)collidablei.hitbox.y);
         
-        //;
+        //Rectangle of the second entity
+        Rectangle secondRect = new Rectangle((int)transformj.position.x, (int)transformj.position.y, (int)collidablej.hitbox.x, (int)collidablej.hitbox.y);
+
         //The center of the first object(x, y, z)
         double firstCenterX = transformi.position.x + collidablei.hitbox.x / 2;
-        //System.out.println(e.getName());
-        //System.out.println(e2.getName());
+;
         double firstCenterY = transformi.position.y + collidablei.hitbox.y / 2;
         double firstCenterZ;
         
@@ -142,133 +133,91 @@ public class CollisionSystem extends SystemJob{
         //The length of the first object in the y axis from the center to the border
         double firstLengthY = collidablei.hitbox.y / 2;
 
-        
         //The length of the first object in the z axis from the center to the border
         double firstLengthZ;
-        
+
         //The center of the second object(x, y, z)
         double secondCenterX = transformj.position.x + collidablej.hitbox.x / 2;
         double secondCenterY = transformj.position.y + collidablej.hitbox.y / 2;
         double secondCenterZ;
 
-        
-        
         //The length of the second object in the x axis from the center to the border
         double secondLengthX = collidablej.hitbox.x / 2;
-        
+
         //The length of the second object in the y axis from the center to the border
         double secondLengthY = collidablej.hitbox.y / 2;
 
-        
         //The length of the second object in the z axis from the center to the border
         double secondLengthZ;
-        
-        //Point of the upper left corner of the first object
-        Point firstPoint1 = new Point(firstCenterX - firstLengthX, firstCenterY - firstLengthY);
-        
-        //Point of the upper right corner of the first object
-        Point firstPoint2 = new Point(firstCenterX + firstLengthX, firstCenterY - firstLengthY);
-        
-        //Point of the bottom left corner of the first object
-        Point firstPoint3 = new Point(firstCenterX - firstLengthX, firstCenterY + firstLengthY);
-        
-        //Point of the bottom right corner of the first object
-        Point firstPoint4 = new Point(firstCenterX + firstLengthX, firstCenterY + firstLengthY);
-        
-        //Point of the upper left corner of the first object
-        Point secondPoint1 = new Point(secondCenterX - secondLengthX, secondCenterY - secondLengthY);
-        
-        //Point of the upper right corner of the first object
-        Point secondPoint2 = new Point(secondCenterX + secondLengthX, secondCenterY - secondLengthY);
-        
-        //Point of the bottom left corner of the first object
-        Point secondPoint3 = new Point(secondCenterX - secondLengthX, secondCenterY + secondLengthY);
-        
-        //Point of the bottom right corner of the first object
-        Point secondPoint4 = new Point(secondCenterX + secondLengthX, secondCenterY + secondLengthY);
-        
-        bRight = (Point.isBetween(firstPoint1, secondPoint1, secondPoint2, 'x')
-                && (Point.isBetween(firstPoint2, secondPoint3, secondPoint1, 'y') || 
-                Point.isBetween(firstPoint4, secondPoint3, secondPoint1, 'y')))
-                && firstPoint1.x - secondPoint4.x <= colMargin;
-        
-        bLeft = (Point.isBetween(firstPoint2, secondPoint1, secondPoint2, 'x')
-                && (Point.isBetween(firstPoint2, secondPoint3, secondPoint1, 'y') || 
-                Point.isBetween(firstPoint4, secondPoint3, secondPoint1, 'y')))
-                && secondPoint3.x - firstPoint2.x <= colMargin;
-        
-        bUp = ((Point.isBetween(firstPoint3, secondPoint1, secondPoint2, 'x') ||
-                Point.isBetween(firstPoint4, secondPoint1, secondPoint2, 'x')) 
-                && Point.isBetween(firstPoint3, secondPoint3, secondPoint1, 'y'))
-                && firstPoint3.y - secondPoint1.y <= colMargin;
-        
-        bDown = ((Point.isBetween(firstPoint3, secondPoint1, secondPoint2, 'x') ||
-                Point.isBetween(firstPoint4, secondPoint1, secondPoint2, 'x')) 
-                && Point.isBetween(firstPoint1, secondPoint3, secondPoint1, 'y'))
-                && secondPoint3.y - firstPoint1.y <= colMargin;
-        
-        
-        if (bDown) {
-            System.out.println("collision down");
 
+        if (firstRect.intersects(secondRect)) {
+
+            //Point of the upper left corner of the first object
+            Point firstPoint1 = new Point(firstCenterX - firstLengthX, firstCenterY - firstLengthY);
+
+            //Point of the upper right corner of the first object
+            Point firstPoint2 = new Point(firstCenterX + firstLengthX, firstCenterY - firstLengthY);
+
+            //Point of the bottom left corner of the first object
+            Point firstPoint3 = new Point(firstCenterX - firstLengthX, firstCenterY + firstLengthY);
+
+            //Point of the bottom right corner of the first object
+            Point firstPoint4 = new Point(firstCenterX + firstLengthX, firstCenterY + firstLengthY);
+
+            //Point of the upper left corner of the first object
+            Point secondPoint1 = new Point(secondCenterX - secondLengthX, secondCenterY - secondLengthY);
+
+            //Point of the upper right corner of the first object
+            Point secondPoint2 = new Point(secondCenterX + secondLengthX, secondCenterY - secondLengthY);
+
+            //Point of the bottom left corner of the first object
+            Point secondPoint3 = new Point(secondCenterX - secondLengthX, secondCenterY + secondLengthY);
+
+            //Point of the bottom right corner of the first object
+            Point secondPoint4 = new Point(secondCenterX + secondLengthX, secondCenterY + secondLengthY);
+            
+            /*
+                Distance in the x axis between the ideally closest pair of points of both entities.
+                Assuming that the first entity is to the left of the second*/
+            int A = abs((int) (secondPoint1.x - firstPoint2.x));
+            
+            
+            /*
+                Distance in the x axis between the ideally closest pair of points of both entities.
+                Assuming that the first entity is to the right of the second*/
+            int B = abs((int) (firstPoint1.x - secondPoint2.x));
+            
+            /*
+                Distance in the y axis between the ideally closest pair of points of both entities.
+                Assuming that the first entity is above the second*/              
+            int C = abs((int) (secondPoint1.y - firstPoint3.y));
+            
+            /*
+                Distance in the y axis between the ideally closest pair of points of both entities.
+                Assuming that the first entity is below the second*/
+            int D = abs((int) (firstPoint1.y - secondPoint3.y));
+
+            //entity1 collides in the left border of entity2
+            if (A < B && A <= min(C, D)) {
+                System.out.println("collision left");
+            } //if entity1 collides in the right border of entity2
+            else if (B < A && B <= min(C, D)) {
+                System.out.println("collision right");
+            }
+            
+            //entity1 collides in the upper limit of entity2
+            else if( C < D) {
+                System.out.println("collision up");
+            }
+            
+            //else if( D < C) (e1 collides in the bottom limit of e2)
+            else {
+                System.out.println("collision down");
+            }
             System.out.println(e.getName() + ": " + transformi.position.x + ", " + transformi.position.y);
-            firstPoint1.print();
-            firstPoint2.print();
-            firstPoint3.print();
-            firstPoint4.print();
             System.out.println(e2.getName() + ": " + transformj.position.x + ", " + transformj.position.y);
-            secondPoint1.print();
-            secondPoint2.print();
-            secondPoint3.print();
-            secondPoint4.print();
             System.out.println("");
         }
-
-        if (bUp) {
-            System.out.println("collision up");
-            System.out.println(e.getName() + ": " + transformi.position.x + ", " + transformi.position.y);
-            firstPoint1.print();
-            firstPoint2.print();
-            firstPoint3.print();
-            firstPoint4.print();
-            System.out.println(e2.getName() + ": " + transformj.position.x + ", " + transformj.position.y);
-            secondPoint1.print();
-            secondPoint2.print();
-            secondPoint3.print();
-            secondPoint4.print();
-            System.out.println("");
-        }
-
-        if (bRight) {
-            System.out.println("collision right");
-            System.out.println(e.getName() + ": " + transformi.position.x + ", " + transformi.position.y);
-            firstPoint1.print();
-            firstPoint2.print();
-            firstPoint3.print();
-            firstPoint4.print();
-            System.out.println(e2.getName() + ": " + transformj.position.x + ", " + transformj.position.y);
-            secondPoint1.print();
-            secondPoint2.print();
-            secondPoint3.print();
-            secondPoint4.print();
-            System.out.println("");
-        }
-        
-        if (bLeft) {
-            System.out.println("collision left");
-            System.out.println(e.getName() + ": " + transformi.position.x + ", " + transformi.position.y);
-            firstPoint1.print();
-            firstPoint2.print();
-            firstPoint3.print();
-            firstPoint4.print();
-            System.out.println(e2.getName() + ": " + transformj.position.x + ", " + transformj.position.y);
-            secondPoint1.print();
-            secondPoint2.print();
-            secondPoint3.print();
-            secondPoint4.print();
-            System.out.println("");
-        }
-
     }
     
     public void initializeEntities() {
